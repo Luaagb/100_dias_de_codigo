5d54e049c1b1130b8d0924967981d26fd385f9bc
Luaagb <barros.lg23@gmail.com>
2024-09-07 18:25:36 -0300

    dia4

---
+++ Dia 4/dia4.py
@@ -0,0 +1,23 @@
+def adicao(x,y):
+    return x+y
+
+def subtracao(x,y):
+    return x-y
+
+def multiplicacao(x,y):
+    return x*y
+
+def divisao(x,y):
+    return x/y
+
+def calculadora():
+    print("Selecione a operação")
+    print("1.Adicao")
+    print("2.Subtracao")
+    print("3.Mutiplicacao")
+    print("4.Divisao")
+
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Dgite o segundo número"))
+
+    
